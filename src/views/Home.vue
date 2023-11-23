373d6a6028792c601eda9913eef5aa336dd440cd
Lea <lea.giangrosso@gmail.com>
2023-11-23 10:54:57 +0100

    Created navBar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,12 +14,21 @@
         {{ letter }}
       </router-link>
     </div>
+    <pre>{{ ingredients }}</pre>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient";
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+  ingredients.value = response.data;
+});
 </script>
