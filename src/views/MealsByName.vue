663c2f0bf153061e80b91e587079aaf7faf71e82
Lea <lea.giangrosso@gmail.com>
2023-11-24 15:24:53 +0100

    Added api search meals and css

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>By Name</div>
+  <div class="p-8 pb-0">
+    <input
+      v-model="keyword"
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+  </div>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <router-link to="/">
+        <img
+          :src="meal.strMealThumb"
+          :alt="strMeal"
+          class="rounded-t-xl h-48 object-cover w-full"
+        />
+      </router-link>
+
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+          recusandae asperiores accusantium quos aliquam ab, vitae obcaecati
+          omnis, odio fugit excepturi culpa voluptatem distinctio molestias, est
+          facilis molestiae officia quasi.
+        </p>
+        <div class="flex items-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+            >Youtube</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "@vue/reactivity";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
+
+const route = useRoute();
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch("searchMeals", keyword.value);
+}
+
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
+</script>
